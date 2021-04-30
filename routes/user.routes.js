0d1f2017985cb58e6174e7159005cead6536a885
unknown <kowalczyk.przemek@wp.pl>
2021-04-30 22:28:25 +0200

    Add subpages: user/profile & profile/settings

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,6 +5,14 @@ router.get('/logged', (req, res) => {
   res.render('logged');
 });
 
+router.get('/profile', (req, res) => {
+  req.user ? res.render('profile') : res.redirect('noPermission');
+});
+
+router.get('/profile/settings', (req, res) => {
+  req.user ? res.render('settings') : res.redirect('noPermission');
+});
+
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 });
